85ac73953b52af1f1c0a14042830b7f36f8ef8e3
noe022 <noeliaperezmojica@gmail.com>
2026-08-17 19:41:33 +0100

    Erase canvas button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("canvas");
-const contex = canvas.getContext('2d');
+const context = canvas.getContext('2d');
+const rubber = document.getElementById("rubber");
 
 let initialX;
 let initialY;
@@ -7,20 +8,20 @@ let initialY;
 // Método que nos permite iniciar un nuevo trazo
 // Para poder dibujar en diferentes partes del lienzo
 function draw(cursorX, cursorY) {
-  contex.beginPath();
+  context.beginPath();
   // Comenzar el trazo
-  contex.moveTo(initialX, initialY);
+  context.moveTo(initialX, initialY);
   // Propiedades del pincel
-  contex.lineWidth = 5;
+  context.lineWidth = 5;
   // Color
-  contex.strokeStyle = "#000";
+  context.strokeStyle = "#000";
   // Terminaciones del pincel
-  contex.lineCap = "round";
-  contex.lineJoin = "round";
+  context.lineCap = "round";
+  context.lineJoin = "round";
   // Mover el trazo
-  contex.lineTo(cursorX, cursorY);
+  context.lineTo(cursorX, cursorY);
   // Dibujar el trazo
-  contex.stroke();
+  context.stroke();
 
   initialX = cursorX;
   initialY = cursorY;
@@ -42,8 +43,10 @@ function mouseUp(evt) {
   canvas.removeEventListener("mousemove", mouseMoving);
 }
 
+function erase() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 canvas.addEventListener('mousedown', mouseClick);
 canvas.addEventListener('mouseup', mouseUp);
-
-
-// Implement rubber
+rubber.addEventListener('mousedown', erase);
